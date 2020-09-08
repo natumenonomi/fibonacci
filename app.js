c337ad622f4ef9f88bacb1904145a39f7096d93b
natumenonomi <nonegemu3e@gmail.com>
2020-09-08 19:45:03 +0900

    指数オーダーのfib関数の実装

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-'use strict';//strict（厳格）モード
-const memo = new Map(); //memoにnewMapとして設定
-memo.set(0,0); //nが0の時0を返す
-memo.set(1,1); //nが1の時1を返す
-function fib(n){ //fib関数の宣言
-  if(memo.has(n)){　
-    return memo.get(n);
+'use strict';
+function fib(n) {
+  if (n === 0) {
+    return 0;
+  } else if (n === 1) {
+    return 1;
   }
-  const value=fib(n-1)+fib(n-2);
-  memo.set(n,value);
-  return value;
+  return fib(n - 1) + fib(n - 2);
 }
-
-const length =40;　//40回繰り返しを宣言
-for( let i =0; i<= length; i ++){
-  //初期化式:0はじまり 条件式：繰り返しの数はlength(今回は40回) 加算式:1増やす
-  console.log(fib(i));//for文のループの実行結果を表示
+const length = 40;
+for (let i = 0; i <= length; i++) {
+  console.log(fib(i));
 }
